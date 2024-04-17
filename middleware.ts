feb5e8491d870e4aaf8d367b899a8df727615a45
4iLeRmax <max.zlatin@gmail.com>
2024-04-17 11:14:42 +0300

    upd

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,21 @@
-// export { default } from 'next-auth/middleware';
+export { default } from 'next-auth/middleware';
 
-import { withAuth, type NextRequestWithAuth } from 'next-auth/middleware';
-import { NextResponse } from 'next/server';
+// import { withAuth, type NextRequestWithAuth } from 'next-auth/middleware';
+// import { NextResponse } from 'next/server';
 
-export default withAuth(
-  function middleware(req: NextRequestWithAuth) {
-    if (req.nextUrl.pathname.startsWith('/dashboard')) {
-      if (req.nextauth.token?.role !== 'ADMIN') {
-        return NextResponse.redirect(new URL('/', req.url));
-      }
-    }
-  },
-  {
-    callbacks: {
-      authorized: ({ token }) => !!token,
-    },
-  },
-);
+// export default withAuth(
+//   function middleware(req: NextRequestWithAuth) {
+//     if (req.nextUrl.pathname.startsWith('/dashboard')) {
+//       if (req.nextauth.token?.role !== 'ADMIN') {
+//         return NextResponse.redirect(new URL('/', req.url));
+//       }
+//     }
+//   },
+//   {
+//     callbacks: {
+//       authorized: ({ token }) => !!token,
+//     },
+//   },
+// );
 
 export const config = { matcher: ['/favorites', '/dashboard'] };
