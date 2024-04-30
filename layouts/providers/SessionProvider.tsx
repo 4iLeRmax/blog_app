7622b77f42656d22796d09486dce8faac640fd98
4iLeRmax <max.zlatin@gmail.com>
2024-04-30 15:50:02 +0300

    last update

--- layouts/providers/SessionProvider.tsx
+++ layouts/providers/SessionProvider.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import { SessionProvider } from 'next-auth/react';
 
 export default function SProvider({ children }: { children: React.ReactNode }) {
-  return <SessionProvider basePath='/api/auth/'>{children}</SessionProvider>;
+  return <>{children}</>;
 }
